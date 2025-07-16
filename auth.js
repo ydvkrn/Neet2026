505f44b998c187451ac1ac18ac9c6a5554aad223
ydvkrn <kyadav00011199@gmail.com>
2025-07-16 11:24:24 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,4 +1,21 @@
-// auth.js
-if (localStorage.getItem("isLoggedIn") !== "true") {
-  window.location.href = "login.html";
-}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBosCBTgIDeRabFMkuqS_cbjkdHDcegFJw",
+  authDomain: "msmneet1.firebaseapp.com",
+  projectId: "msmneet1",
+  storageBucket: "msmneet1.firebasestorage.app",
+  messagingSenderId: "920361933770",
+  appId: "1:920361933770:web:4dba802da04a8548348d33",
+  measurementId: "G-RWVBLW9YLK"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, user => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
